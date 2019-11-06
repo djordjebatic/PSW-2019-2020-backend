91cb759f1f1fe0350c3b0816a4a99c2f020a52f2
Jelena Dostic <jelenadostic2@gmail.com>
2019-11-06 19:45:19 +0100

    adding initial project

---
+++ psw-backend/src/main/java/com/example/pswbackend/PswBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.pswbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PswBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PswBackendApplication.class, args);
+	}
+
+}
