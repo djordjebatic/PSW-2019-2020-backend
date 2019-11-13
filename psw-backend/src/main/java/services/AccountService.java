cada754b0e7b1702a2c4b405124c03adc0fd7610
SaraFojkar <56953463+SaraFojkar@users.noreply.github.com>
2019-11-13 19:13:31 +0100

    added signUpUser method

--- psw-backend/src/main/java/services/AccountService.java
+++ psw-backend/src/main/java/services/AccountService.java
@@ -2,9 +2,13 @@ package services;
 
 import domain.Account;
 import dto.UserLoginDTO;
+import dto.UserSignUpDTO;
+
 
 public interface AccountService {
 
     Account loginUser(UserLoginDTO loginDTO);
-
+    
+    Account signUpUser(UserSignUpDTO signUpDTO);
+    
 }
