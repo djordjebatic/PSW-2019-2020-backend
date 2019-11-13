f0fce14475d5583297efc26d1e38d115a6e4dab8
Jelena Dostić <49482144+DosticJelena@users.noreply.github.com>
2019-11-13 20:47:14 +0100

    updating AccountService

--- psw-backend/src/main/java/services/AccountServiceImpl.java
+++ psw-backend/src/main/java/services/AccountServiceImpl.java
@@ -11,9 +11,18 @@ import repositories.AccountRepository;
 @Service
 public class AccountServiceImpl implements AccountService {
 
+<<<<<<< Updated upstream
     public Account loginUser(UserLoginDTO loginDTO){
 
         Account foundAccount = AccountRepository.findOne(loginDTO.getEmailAddress());
+=======
+    @Autowired
+    private AccountRepository accountRepo;
+	
+    public Account loginUser(UserLoginDTO loginDTO){
+
+        Account foundAccount = accountRepo.findOne(loginDTO.getEmailAddress());
+>>>>>>> Stashed changes
 
         if (foundAccount.getPassword() != loginDTO.getPassword()){
             return null;
@@ -22,7 +31,25 @@ public class AccountServiceImpl implements AccountService {
         return foundAccount;
     }
     
+<<<<<<< Updated upstream
     public Account signUpUser(UserSignUpDTO signUpDTO) {
+=======
+    public Patient signUpUser(UserSignUpDTO signUpDTO) {
+    	
+    	Account foundAccount = accountRepo.findOne(signUpDTO.getEmail());
+    	
+    	Patient newPatient= new Patient();
+   
+    	newPatient.setEmail(signUpDTO.getEmail());
+    	newPatient.setPassword(signUpDTO.getPassword());
+    	newPatient.setFirstName(signUpDTO.getFirstName());
+    	newPatient.setLastName(signUpDTO.getLastName());
+    	newPatient.setAddress(signUpDTO.getAddress());
+    	newPatient.setCity(signUpDTO.getCity());
+    	newPatient.setCountry(signUpDTO.getCountry());
+    	newPatient.setPhoneNumber(signUpDTO.getPhoneNumber());
+    	newPatient.setMedicalNumber(signUpDTO.getMedicalNumber());
+>>>>>>> Stashed changes
     	
     	return null;
     }
