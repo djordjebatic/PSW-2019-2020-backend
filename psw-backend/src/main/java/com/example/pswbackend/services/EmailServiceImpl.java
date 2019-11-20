ceaa6d7108c513ca3698fd00079d220e2dafce1f
Djordje Batic <djordjebatic@gmail.com>
2019-11-20 22:08:25 +0100

    Implemented mail notification service

---
+++ psw-backend/src/main/java/com/example/pswbackend/services/EmailServiceImpl.java
@@ -0,0 +1,23 @@
+package com.example.pswbackend.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailServiceImpl implements EmailService {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @Override
+    public void sendEmail(String recipient, String subject, String message) {
+        SimpleMailMessage mail = new SimpleMailMessage();
+        mail.setTo(recipient);
+        mail.setSubject(subject);
+        mail.setText(message);
+
+        javaMailSender.send(mail);
+    }
+}
