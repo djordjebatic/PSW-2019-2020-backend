fe1ccc969d9ce6bb7b9d8a1e4841d36479f4c100
Djordje Batic <djordjebatic@gmail.com>
2019-11-20 12:37:12 +0100

    Changed files in order to implement inheritance

---
+++ psw-backend/src/main/java/com/example/pswbackend/domain/Account.java
@@ -0,0 +1,126 @@
+package com.example.pswbackend.domain;
+
+import org.springframework.lang.NonNull;
+
+import javax.persistence.*;
+import javax.validation.constraints.Pattern;
+import java.util.Set;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name="AccountType")
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(columnDefinition = "VARCHAR(30)", nullable = false)
+    private String firstName;
+
+    @Column(columnDefinition = "VARCHAR(30)", nullable = false)
+    private String lastName;
+
+    @Column(columnDefinition = "VARCHAR(11)", unique = true, nullable = false)
+    @Pattern(regexp = "0[0-9]+")
+    private String phoneNumber;
+
+    @Column(columnDefinition = "VARCHAR(50)", nullable = false)
+    private String address;
+
+    @Column(columnDefinition = "VARCHAR(30)", nullable = false)
+    private String city;
+
+    @Column(columnDefinition = "VARCHAR(30)", nullable = false)
+    private String country;
+
+    @Column(name = "roles")
+    @NonNull
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private Set<Role> accountRoles;
+
+    public Account(){
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Set<Role> getAccountRoles() {
+        return accountRoles;
+    }
+
+    public void setAccountRoles(Set<Role> accountRoles) {
+        this.accountRoles = accountRoles;
+    }
+}
--- psw-backend/src/main/java/com/example/pswbackend/domain/Doctor.java
+++ psw-backend/src/main/java/com/example/pswbackend/domain/Doctor.java
@@ -12,33 +12,8 @@ import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 @Entity
-public class Doctor{
-
-	@Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-	
-	@NotEmpty(message = "Email is empty.")
-    @Email(message ="Email is invalid.")
-    @Column(unique = true, nullable = false)
-    private String email;
-
-    @Column(nullable = false)
-    private String password;
-    
-    @NotEmpty(message = "First name is empty.")
-    @Column(columnDefinition = "VARCHAR(30)", nullable = false)
-    private String firstName;
-
-    @NotEmpty(message = "Last name is empty.")
-    @Column(columnDefinition = "VARCHAR(30)", nullable = false)
-    private String lastName;
-
-    @NotEmpty(message = "Phone number is empty.")
-    @Size(min=9, max=10)
-    @Pattern(regexp = "0[0-9]+")
-    @Column(columnDefinition = "VARCHAR(11)", unique = true, nullable = false)
-    private String phoneNumber;
+@DiscriminatorValue("Doctor")
+public class Doctor extends Account {
     
 	@ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
 	private Clinic clinic;
@@ -47,12 +22,12 @@ public class Doctor{
     private Set<Appointment> appointments = new HashSet<Appointment>();
 
 	@Enumerated(EnumType.STRING)
-	private UserStatus status;
+	private UserStatus userStatus;
 
-	@Column(nullable = false)
+	@Column
 	private int stars;
 
-	@Column(nullable = false)
+	@Column
 	private int num_votes;
 
 	public int getStars() {
@@ -71,67 +46,7 @@ public class Doctor{
 		this.num_votes = num_votes;
 	}
 
-	public Doctor(@NotEmpty(message = "Email is empty.") @Email(message = "Email is invalid.") String email,
-				  String password, @NotEmpty(message = "First name is empty.") String firstName,
-				  @NotEmpty(message = "Last name is empty.") String lastName,
-				  @NotEmpty(message = "Phone number is empty.") @Size(min = 9, max = 10) @Pattern(regexp = "0[0-9]+") String phoneNumber,
-				  Clinic clinic, Set<Appointment> appointments) {
-		super();
-		this.email = email;
-		this.password = password;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.phoneNumber = phoneNumber;
-		this.clinic = clinic;
-		this.appointments = appointments;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getPhoneNumber() {
-		return phoneNumber;
-	}
-
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
+	public Doctor(){
 	}
 
 	public Clinic getClinic() {
@@ -151,10 +66,10 @@ public class Doctor{
 	}
 
 	public UserStatus getStatus() {
-		return status;
+		return userStatus;
 	}
 
-	public void setStatus(UserStatus status) {
-		this.status = status;
+	public void setStatus(UserStatus userStatus) {
+		this.userStatus = userStatus;
 	}
 }
