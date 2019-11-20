729414e89f2d3b6a3af9c64827a95eb89c080771
Jelena Dostić <49482144+DosticJelena@users.noreply.github.com>
2019-11-20 10:07:04 +0100

    creating login request action

--- src/Redux/Actions.js
+++ src/Redux/Actions.js
@@ -1,2 +1,13 @@
 const LOG_IN = 'LOG_IN'
 const SIGN_UP = 'SIGN_UP'
+
+// -----------------------------------
+
+export const login = (email, password) => {
+    return dispatch => {
+      axios.put("https://localhost:44302/api/editTask?emailAddress=" + email) // SREDITI URL
+      .then(response => {
+        // srediti state!
+      });
+    };
+};
