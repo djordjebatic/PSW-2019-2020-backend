c507d72574296a5e115f998f10db1c453d5b2024
Jelena Dostić <49482144+DosticJelena@users.noreply.github.com>
2019-12-02 21:37:53 +0100

    added basic methods to doctor controller/service

--- psw-backend/src/main/java/com/example/pswbackend/services/DoctorService.java
+++ psw-backend/src/main/java/com/example/pswbackend/services/DoctorService.java
@@ -1,9 +1,16 @@
 package com.example.pswbackend.services;
 
+import com.example.pswbackend.domain.Doctor;
 import com.example.pswbackend.dto.AppointmentDoctorDTO;
+
+import java.util.List;
 
 public interface DoctorService {
 
     boolean sheduleAppointment(AppointmentDoctorDTO dto);
+
+    Doctor findById(long id);
+
+    List<Doctor> findAll();
 
 }
