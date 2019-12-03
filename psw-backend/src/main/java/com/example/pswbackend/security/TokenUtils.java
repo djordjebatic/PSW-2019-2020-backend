1727c72b9ac26d3daa3018b737aa8fba2ebb5125
SaraFojkar <56953463+SaraFojkar@users.noreply.github.com>
2019-12-03 15:26:15 +0100

    added class TokenAuthenticationFilter

--- psw-backend/src/main/java/com/example/pswbackend/security/TokenUtils.java
+++ psw-backend/src/main/java/com/example/pswbackend/security/TokenUtils.java
@@ -1,4 +1,24 @@
 package com.example.pswbackend.security;
 
+import com.sun.org.apache.xml.internal.security.algorithms.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.servlet.http.HttpServletRequest;
+
 public class TokenUtils {
+
+
+    public String getToken(HttpServletRequest request) {
+        return null;
+    }
+
+    public String getUsernameFromToken(String authToken) {
+        return null;
+    }
+
+    public boolean validateToken(String authToken, UserDetails userDetails) {
+        return false;
+    }
 }
