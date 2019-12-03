0afc16052fd0179dd392753e92ab6f41538c9970
Jelena Dostić <49482144+DosticJelena@users.noreply.github.com>
2019-12-03 20:57:19 +0100

    deleted CrossOrigin annotations

--- psw-backend/src/main/java/com/example/pswbackend/controllers/DoctorController.java
+++ psw-backend/src/main/java/com/example/pswbackend/controllers/DoctorController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api")
 public class DoctorController {
