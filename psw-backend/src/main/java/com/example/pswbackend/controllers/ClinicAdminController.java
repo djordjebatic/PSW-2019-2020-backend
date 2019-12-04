f91afd09c28dac6879edbcfe2143aae5adb546ef
Jelena Dostić <49482144+DosticJelena@users.noreply.github.com>
2019-12-04 13:33:33 +0100

    created ClinicAdminController
    - method createQuickReservation

---
+++ psw-backend/src/main/java/com/example/pswbackend/controllers/ClinicAdminController.java
@@ -0,0 +1,35 @@
+package com.example.pswbackend.controllers;
+
+import com.example.pswbackend.domain.Account;
+import com.example.pswbackend.domain.Appointment;
+import com.example.pswbackend.dto.QuickReservationDTO;
+import com.example.pswbackend.services.ClinicAdminService;
+import com.example.pswbackend.services.DoctorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/api/clinic-admin")
+public class ClinicAdminController {
+
+    @Autowired
+    private ClinicAdminService clinicAdminService;
+
+    @PostMapping(value="/quick-reservation", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Appointment> createQuickReservation(QuickReservationDTO dto){
+
+        Appointment appointment = clinicAdminService.createQuickReservation(dto);
+
+        if(appointment == null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST); // izmeniti tip statusa
+        }
+
+        return new ResponseEntity<>(appointment, HttpStatus.CREATED);
+    }
+
+}
