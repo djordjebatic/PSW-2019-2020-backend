14290a971745bd12c6e7136fa8ad5b01d526f11c
Jelena Dostić <49482144+DosticJelena@users.noreply.github.com>
2019-12-04 13:32:50 +0100

    udpated QuickReservationDTO

--- psw-backend/src/main/java/com/example/pswbackend/dto/QuickReservationDTO.java
+++ psw-backend/src/main/java/com/example/pswbackend/dto/QuickReservationDTO.java
@@ -1,14 +1,20 @@
 package com.example.pswbackend.dto;
 
+import com.example.pswbackend.domain.ClinicAdmin;
+import com.example.pswbackend.domain.Doctor;
+import com.example.pswbackend.domain.Ordination;
+
 public class QuickReservationDTO {
 
-    private long doctor;
-    private long ordination;
+    //private ClinicAdmin clinicAdmin; //TODO dodati *koji* admin pravi brzu rezervaciju
+    private Long doctor;
+    private Long ordination;
     private String date;
     private String time;
     private float price;
     private int duration; // u minutima
     private String type; //  0 = EXAMINATION, 1 = OPERATION
+
 
     public long getDoctor() { return doctor; }
 
