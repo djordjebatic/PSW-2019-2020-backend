f121b83976a371f3243d851217d38b74355cd7b4
SaraFojkar <56953463+SaraFojkar@users.noreply.github.com>
2019-12-04 09:52:03 +0100

    layout for patients personal page

--- src/App/Content/PersonalProfile/PersonalProfile.js
+++ src/App/Content/PersonalProfile/PersonalProfile.js
@@ -1,16 +1,68 @@
 import React from 'react';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
-
+import'./PersonalProfile.css'
 
 class PersonalProfile extends React.Component {
+ 
+ construct(props){
+   
+   this.state={
+
+   }
+ }
+ 
+ 
   render() {
     
   return (
     <div className="PersonalProfile">
       <Header/>
       <br/>
-      <h3>Personal Profile Page</h3>
+      <h3 >Personal page</h3>
+      <br/>
+        <div className="info">
+          <div className="form-group">
+            <label>First name: </label>
+
+
+          </div>
+          <div className="form-group">
+            <label>Last name: </label>
+
+
+          </div>
+          <div className="form-group">
+            <label>First name: </label>
+
+
+          </div>
+          <div className="form-group">
+            <label>Medical number: </label>
+
+
+          </div>
+          <div className="form-group">
+            <label>Address: </label>
+
+
+          </div>
+          <div className="form-group">
+            <label>City: </label>
+
+
+          </div>
+          <div className="form-group">
+            <label>Country: </label>
+
+
+          </div>
+          <div className="form-group">
+            <label>Phone number: </label>
+
+
+          </div>
+      </div>
       <br/>
       <Footer/>
 
