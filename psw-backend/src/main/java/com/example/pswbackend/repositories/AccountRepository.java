0dd8c09266adce84b6561c26795ee9b3935a15e6
Djordje Batic <djordjebatic@gmail.com>
2019-12-04 19:26:35 +0100

    Created AccountRepository

---
+++ psw-backend/src/main/java/com/example/pswbackend/repositories/AccountRepository.java
@@ -0,0 +1,10 @@
+package com.example.pswbackend.repositories;
+import com.example.pswbackend.domain.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    Account findByEmail(String email);
+    //List<Account> findByStatus(UserStatus userStatus);
+
+}
