6ef21ce6ee3620165c0784f67d3502de03452786
Jelena Dostić <49482144+DosticJelena@users.noreply.github.com>
2019-12-06 18:20:14 +0100

    changed DiscriminatorColumn annotation
    - added discriminator type, and value

--- psw-backend/src/main/java/com/example/pswbackend/domain/Account.java
+++ psw-backend/src/main/java/com/example/pswbackend/domain/Account.java
@@ -14,7 +14,8 @@ import java.util.List;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name="AccountType")
+@DiscriminatorColumn(name="discriminator", discriminatorType = DiscriminatorType.STRING)
+@DiscriminatorValue(value = "ACCOUNT")
 public class Account implements UserDetails {
 
     @Id
