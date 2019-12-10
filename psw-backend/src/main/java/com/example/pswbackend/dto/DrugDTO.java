130a569e69b66c4dda9570b91750016b1625462b
Djordje Batic <djordjebatic@gmail.com>
2019-12-10 22:13:19 +0100

    Create DrugDTO.java

---
+++ psw-backend/src/main/java/com/example/pswbackend/dto/DrugDTO.java
@@ -0,0 +1,41 @@
+package com.example.pswbackend.dto;
+
+public class DrugDTO {
+    private Long id;
+
+    private String name;
+
+    private String ingredient;
+
+    private String description;
+
+    public DrugDTO(String name, String ingredient, String description) {
+        this.name = name;
+        this.ingredient = ingredient;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(String ingredient) {
+        this.ingredient = ingredient;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
