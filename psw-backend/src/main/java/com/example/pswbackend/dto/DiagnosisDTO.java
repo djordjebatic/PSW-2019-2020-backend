d7e4b18f777c31a3ee93989f4a03e246bacefd71
Djordje Batic <djordjebatic@gmail.com>
2019-12-10 22:14:03 +0100

    Create DiagnosisDTO.java

---
+++ psw-backend/src/main/java/com/example/pswbackend/dto/DiagnosisDTO.java
@@ -0,0 +1,29 @@
+package com.example.pswbackend.dto;
+
+public class DiagnosisDTO {
+
+    private Long id;
+    private String name;
+    private String description;
+
+    public DiagnosisDTO(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
