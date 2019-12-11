132618279eb16166765042e782422ba129751bb6
Jelena Dostić <49482144+DosticJelena@users.noreply.github.com>
2019-12-11 13:52:17 +0100

    added new accounts for testing

--- psw-backend/src/main/resources/data-postgres.sql
+++ psw-backend/src/main/resources/data-postgres.sql
@@ -23,28 +23,37 @@ insert into account (account_type, email, password, first_name, last_name, phone
 values ('CLINIC_ADMIN', 'cadmin1@gmail.com', '$2y$12$4zrqOojpixOe/ogFw1xyyuQuIvFqrzbj0IohYtshqqy1P5rS6kdbq', 'Marko', 'Marković', '066123456', 'Zeleznicka ulica 69','Ndzamena','Chad', 'ACTIVE', 1);
 INSERT INTO account_authority (account_id, authority_id) values  (2, 4);
 
+insert into account (account_type, email, password, first_name, last_name, phone_number, address, city, country, user_status, clinic_id)
+values ('CLINIC_ADMIN', 'cadmin2@gmail.com', '$2y$12$4zrqOojpixOe/ogFw1xyyuQuIvFqrzbj0IohYtshqqy1P5rS6kdbq', 'Milos', 'Marković', '066123457', 'Zeleznicka ulica 69','Ndzamena','Chad', 'NEVER_LOGGED_IN', 1);
+INSERT INTO account_authority (account_id, authority_id) values  (3, 4);
+
+
 --Doctor
 insert into account(account_type, email, password, first_name, last_name, phone_number, address, city, country, clinic_id, user_status, stars, num_votes)
 values ('DOCTOR', 'dok@gmail.com', '$2y$12$u3nc1wRBsop15oZaI2FqVuSHFsD9ZHeGXcpXKwGeD3on4zv3BRWd6', 'Dok', 'Dokic', '065525404', 'Zeleznicka ulica 69','Ndzamena','Chad', 1, 'ACTIVE', 50, 10);
-INSERT INTO account_authority (account_id, authority_id) values  (3, 2);
+INSERT INTO account_authority (account_id, authority_id) values  (4, 2);
 
 insert into account(account_type, email, password, first_name, last_name, phone_number, address, city, country, clinic_id, user_status, stars, num_votes)
 values ('DOCTOR', 'dok2@gmail.com', '$2y$12$hjx/YFOIKdJhYhnoabYN5enQVYWQvrOU/h/NyT8jK4/af5LWDoKd2', 'Sima', 'Dokic', '060123456', 'Zeleznicka ulica 69','Ndzamena','Chad', 1, 'NEVER_LOGGED_IN', 46, 10);
-INSERT INTO account_authority (account_id, authority_id) values  (4, 2);
+INSERT INTO account_authority (account_id, authority_id) values  (5, 2);
 
 --Nurse
 insert into account(account_type, email, password, first_name, last_name, phone_number, address, city, country, clinic_id, user_status)
 values ('NURSE', 'sestra@gmail.com', '$2y$12$M6EnOZMRiIFt/znBY/C8r.2sglykYnJ0jj2Zm.mGpB3mdG9hD0BeW', 'Sima', 'Simic', '065256155', 'Zeleznicka ulica 69','Ndzamena','Chad', '1','ACTIVE');
-INSERT INTO account_authority (account_id, authority_id) values  (5, 3);
+INSERT INTO account_authority (account_id, authority_id) values  (6, 3);
+
+insert into account(account_type, email, password, first_name, last_name, phone_number, address, city, country, clinic_id, user_status)
+values ('NURSE', 'sestra2@gmail.com', '$2y$12$M6EnOZMRiIFt/znBY/C8r.2sglykYnJ0jj2Zm.mGpB3mdG9hD0BeW', 'Sestra', 'Simic', '065365157', 'Zeleznicka ulica 82','Ndzamena','Chad', '1','NEVER_LOGGED_IN');
+INSERT INTO account_authority (account_id, authority_id) values  (7, 3);
 
 --Patient
 insert into account (account_type, email, password, first_name, last_name, phone_number, address, city, country, medical_number, status)
 values ('PATIENT', 'patijent@gmail.com', '$2y$12$cfJ7eUBHua9B4mJhHXAx2eN6j/6sfGduRWRyMQy7E/Gci0xUXt8tK', 'Pata', 'Patic', '065256165', 'Zeleznicka ulica 69','Ndzamena','Chad','901204931212','APPROVED');
-INSERT INTO account_authority (account_id, authority_id) values  (6, 1);
+INSERT INTO account_authority (account_id, authority_id) values  (8, 1);
 
 insert into account (account_type, email, password, first_name, last_name, phone_number, address, city, country, medical_number, status)
 values ('PATIENT', 'patijent2@gmail.com', '$2y$12$cfJ7eUBHua9B4mJhHXAx2eN6j/6sfGduRWRyMQy7E/Gci0xUXt8tK', 'Pera', 'Peric', '064123466', 'Veselina Maslese 3','Beograd','Srbija','1234567890123','AWAITING_APPROVAL');
-INSERT INTO account_authority (account_id, authority_id) values  (7, 1);
+INSERT INTO account_authority (account_id, authority_id) values  (9, 1);
 
 ------------------------------------------------------------------------------
 
