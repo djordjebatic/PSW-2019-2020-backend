bddcd9648e71e27c21af0d784c4763e70475d159
SaraFojkar <56953463+SaraFojkar@users.noreply.github.com>
2019-12-11 02:11:59 +0100

    Added PredefinedAppointmentService and PredefinedAppointmentServiceImpl

---
+++ psw-backend/src/main/java/com/example/pswbackend/services/PredefinedAppointmentService.java
@@ -0,0 +1,13 @@
+package com.example.pswbackend.services;
+
+import com.example.pswbackend.domain.Appointment;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface PredefinedAppointmentService{
+
+    Appointment findById(Long id);
+    List<Appointment> findAll();
+}
---
+++ psw-backend/src/main/java/com/example/pswbackend/services/PredefinedAppointmentServiceImpl.java
@@ -0,0 +1,24 @@
+package com.example.pswbackend.services;
+import com.example.pswbackend.domain.Appointment;
+
+import java.util.List;
+
+
+public class PredefinedAppointmentServiceImpl implements PredefinedAppointmentService{
+
+    @Override
+    public Appointment findById(Long id){
+
+        Appointment appointment;
+
+        return null;
+    }
+
+    @Override
+    public List<Appointment> findAll(){
+
+        return null;
+    }
+
+
+}
