237b54f40420e3f2ea0f9a6287352cf732ee4132
Jelena Dostić <49482144+DosticJelena@users.noreply.github.com>
2019-12-11 20:16:50 +0100

    added clinic admin service implementation test methods
    missing implementation

---
+++ psw-backend/src/test/java/com/example/pswbackend/services/ClinicAdminServiceImplTest.java
@@ -0,0 +1,18 @@
+package com.example.pswbackend.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ClinicAdminServiceImplTest {
+
+    @Test
+    void createQuickReservation_whenQuickReservationDtoIsNull_ThrowsException(){ // koji exception
+
+    }
+
+    @Test
+    void createQuickReservation_whenQuickReservationDtoIsValid_ReturnsAppointment(){ 
+
+    }
+}
