4e33d702f45dc1d7a9678fe94dbf745157b4896b
Jelena Dostić <49482144+DosticJelena@users.noreply.github.com>
2019-12-14 16:55:23 +0100

    added update Account method

--- psw-backend/src/main/java/com/example/pswbackend/controllers/AccountController.java
+++ psw-backend/src/main/java/com/example/pswbackend/controllers/AccountController.java
@@ -1,7 +1,9 @@
 package com.example.pswbackend.controllers;
 
 import com.example.pswbackend.domain.Account;
+import com.example.pswbackend.domain.Patient;
 import com.example.pswbackend.dto.LoginDTO;
+import com.example.pswbackend.dto.PatientDTO;
 import com.example.pswbackend.services.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -38,6 +40,27 @@ public class AccountController {
     @PreAuthorize("hasAuthority('ACCOUNT')")
     public Account account(Principal account) {
         return this.accountService.findByEmail(account.getName());
+    }
+
+    @PutMapping(value="/update-personal-info/{id}", produces=MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PreAuthorize("hasRole('DOCTOR') or hasRole('PATIENT') or hasRole('CLINIC_ADMIN') or hasRole('CC_ADMIN') or hasRole('NURSE')")
+    public Account updateAccount(@PathVariable long id,@RequestBody PatientDTO dto){
+
+        Account account= accountService.findById(id);
+
+        if(account==null){
+            return null;
+        }
+
+        account.setFirstName(dto.getFirstName());
+        account.setLastName(dto.getLastName());
+        account.setAddress(dto.getAddress());
+        account.setPhoneNumber(dto.getPhoneNumber());
+        account.setCity(dto.getCity());
+        account.setCountry(dto.getCountry());
+        //account.setPassword(dto.getPassword());
+
+        return account;
     }
 
 }
