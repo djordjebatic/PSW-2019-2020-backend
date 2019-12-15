95f6c962ba7b42092cbec0b2d9e20c6d95854385
Djordje Batic <djordjebatic@gmail.com>
2019-12-15 14:40:39 +0100

    Create RegistrationRequests.js

---
+++ src/App/Content/CCAdmin/RegistationRequests/RegistrationRequests.js
@@ -0,0 +1,151 @@
+import React from 'react';
+import "react-table/react-table.css";
+import ReactTable from "react-table";
+import axios from 'axios';
+import Footer from '../../Footer/Footer';
+import Header from '../../Header/Header';
+import {NotificationManager} from 'react-notifications';
+
+class RegistrationRequests extends React.Component{
+      constructor () {
+          super();
+          this.handleChange = this.handleChange.bind(this);
+          this.handleOpenModal = this.handleOpenModal.bind(this);
+          this.handleCloseModal = this.handleCloseModal.bind(this);
+
+          this.state = {
+              tableData: [
+                {
+                  email: '',
+                  firstName: '',
+                  lastName: '',
+                  address: '',
+                  city: '',
+                  country: '',
+                  phoneNumber: ''
+                }
+                ],
+                showModal: false,
+                message: ""
+          };
+
+      }
+
+      handleChange(e) {
+        this.setState({...this.state, [e.target.name]: e.target.value});
+        console.log(this.state)
+      }
+
+      handleOpenModal () {
+        this.setState({ showModal: true });
+      }
+      
+      handleCloseModal () {
+        this.setState({ showModal: false });
+      }
+
+      componentDidMount () {
+          axios.get('http://localhost:8080/api/cc-admin/all-registration-requests', {
+              responseType: 'json'
+          }).then(response => {
+              this.setState({ tableData: response.data });
+          });
+      }
+
+      accept = (id) =>{
+        console.log(id)
+        axios.put("http://localhost:8080/api/cc-admin/approve-registration-request/" + id).then(response => {
+          const {tableData} = this.state;
+          tableData.pop(response.data);
+          this.setState({tableData});
+        }).then((resp) => {NotificationManager.success('Patient registration request has been sucessfully approved. \n Confirmation email has been sent', '', 3000);}) 
+      }
+
+      reject = (id) =>{
+        this.state.message = "Sorry bro i had to do it"
+        if (this.state.message != ""){
+          axios.put("http://localhost:8080/api/cc-admin/reject-registration-request/" + id, this.state.message).then(response => {
+            const {tableData} = this.state;
+            tableData.pop(response.data);
+            this.setState({tableData});
+          }).then((resp) => NotificationManager.success('Patient registration request has been sucessfully rejected. Rejection email has been sent to patient', '', 3000))
+          }
+        else{
+            NotificationManager.error('Wrong input.', '', 3000)
+          }
+      }
+
+      render() {
+        let { tableData } = this.state;
+        return (
+          <div className="AssignCCAdmin">
+          <Header/>
+          <div className='nonccadmins rtable'>
+          <ReactTable 
+          data={tableData}
+          columns={[{
+                      Header: 'Email',
+                      accessor: 'email',
+                      width: 200
+                    },{
+                      Header: 'First Name',
+                      accessor: 'firstName'
+                    },{
+                      Header: 'Last Name',
+                      accessor: 'lastName'
+                    },
+                    {
+                      Header: 'City',
+                      accessor: 'city'
+                    },
+                    {
+                      Header: 'Country',
+                      accessor: 'country'
+                    },
+                    {
+                      Header: '',
+                      Cell: row => (
+                          <div>
+                              <button className="primary btn" onClick={() => this.accept(row.original.id)}>Accept</button>
+                          </div>
+                      ),
+                      width: 100
+
+                    },
+                    {
+                        Header: '',
+                        Cell: row => (
+                            <div id="row">
+                                <button className="primary btn" onClick={() => this.reject(row.original.id)}>Reject</button>
+                            </div>
+                        ),
+                        width: 100
+
+                      },
+                      {
+                        Header: 'Reason',
+                        Cell: row => (
+                            <div>
+                                <input type="text" 
+                                className="form-control form-control-sm"
+                                id="message"
+                                name="message"
+                                //onChange={this.handleChange}
+                                placeholder="Reason"/>
+                            </div>
+                        ),
+                        width: 250
+  
+                      }
+          ]}
+          defaultPageSize = {10}
+          pageSizeOptions = {[5, 10, 15]}
+          />
+          </div>
+           <Footer/>
+           </div>
+        )
+      }
+    }
+
+export default RegistrationRequests
