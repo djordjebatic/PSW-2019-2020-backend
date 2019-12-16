0c551aa40f39b6f9edc9392c6f19a9bd94802cb2
Djordje Batic <djordjebatic@gmail.com>
2019-12-16 16:37:02 +0100

    Added ModelMapper dependency

--- psw-backend/src/main/java/com/example/pswbackend/PswBackendApplication.java
+++ psw-backend/src/main/java/com/example/pswbackend/PswBackendApplication.java
@@ -1,10 +1,19 @@
 package com.example.pswbackend;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableTransactionManagement
 @SpringBootApplication
 public class PswBackendApplication {
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PswBackendApplication.class, args);
