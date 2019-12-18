92bb81e4a3dc322720eca02690ba97f07051dee8
Djordje Batic <djordjebatic@gmail.com>
2019-12-18 23:58:32 +0100

    Added methods to appointment repository

--- psw-backend/src/main/java/com/example/pswbackend/repositories/AppointmentRepository.java
+++ psw-backend/src/main/java/com/example/pswbackend/repositories/AppointmentRepository.java
@@ -1,8 +1,8 @@
 package com.example.pswbackend.repositories;
 
 import com.example.pswbackend.domain.Appointment;
+import com.example.pswbackend.enums.AppointmentStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
@@ -10,6 +10,12 @@ public interface AppointmentRepository extends JpaRepository<Appointment, Long> 
     Appointment findOneById(Long id);
     List<Appointment> findAll();
     List<Appointment> findByClinicId(Long id);
+
+    List<Appointment> findByDoctorsIdAndStatusNot(Long id, AppointmentStatus appointmentStatus);
+    List<Appointment> findByOrdinationIdAndStatusNotOrderByStartDateTime(Long id, AppointmentStatus appointmentStatus);
+    List<Appointment> findByNurseIdAndStatusNot(Long id, AppointmentStatus appointmentStatus);
+    Appointment getByIdAndStatusNot(Long id, AppointmentStatus appointmentStatus);
+    List<Appointment> findByStatus(AppointmentStatus appointmentStatus);
 
 }
 
