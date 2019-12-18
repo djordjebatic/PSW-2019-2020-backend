3bf64ce99bca38d3c05567c93febcac8eb941b50
Djordje Batic <djordjebatic@gmail.com>
2019-12-19 00:00:04 +0100

    Create AppointmentController.java

---
+++ psw-backend/src/main/java/com/example/pswbackend/controllers/AppointmentController.java
@@ -0,0 +1,84 @@
+package com.example.pswbackend.controllers;
+
+import com.example.pswbackend.domain.Appointment;
+import com.example.pswbackend.domain.Doctor;
+import com.example.pswbackend.services.AppointmentService;
+import com.example.pswbackend.services.DoctorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping(value = "/api/appointment")
+public class AppointmentController {
+
+    @Autowired
+    private AppointmentService appointmentService;
+
+    @Autowired
+    private DoctorService doctorService;
+
+    @GetMapping(value = "/get-awaiting-approval-appointments")
+    @PreAuthorize("hasRole('CLINIC_ADMIN')")
+    public ResponseEntity<List<Appointment>> getAwaitingApprovalAppointments(){
+        return new ResponseEntity<>(appointmentService.getAwaitingApprovalAppointments(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/get-canceled-appointments")
+    @PreAuthorize("hasRole('CLINIC_ADMIN')")
+    public ResponseEntity<List<Appointment>> getCanceledAppointments(){
+        return new ResponseEntity<>(appointmentService.getCanceledAppointments(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/get-predefined-awailable-appointments")
+    @PreAuthorize("hasRole('CLINIC_ADMIN')")
+    public ResponseEntity<List<Appointment>> getPredefinedAwailableAppointments(){
+        return new ResponseEntity<>(appointmentService.getPredefinedAwailableAppointments(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/get-predefined-booked-appointments")
+    @PreAuthorize("hasRole('CLINIC_ADMIN')")
+    public ResponseEntity<List<Appointment>> getPredefinedBookedAppointments(){
+        return new ResponseEntity<>(appointmentService.getPredefinedBookedAppointments(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/get-doctor-appointments")
+    @PreAuthorize("hasRole('DOCTOR')")
+    public ResponseEntity<List<Appointment>> getDoctorAppointments() {
+        Doctor doctor = doctorService.getLoggedInDoctor();
+        try {
+            return new ResponseEntity<>(appointmentService.getDoctorAppointments(doctor.getId()), HttpStatus.OK);
+        }
+        catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping(value = "/get-nurse-appointments/{id}")
+    @PreAuthorize("hasRole('NURSE')")
+    public ResponseEntity<List<Appointment>> getNurseAppointments(@PathVariable Long id) {
+        try {
+            return new ResponseEntity<>(appointmentService.getNurseAppointments(id), HttpStatus.OK);
+        }
+        catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PutMapping("/cancel/{id}")
+    @PreAuthorize("hasRole('DOCTOR')")
+    public ResponseEntity<Appointment> cancelAppointments(@PathVariable Long id){
+        /*try {
+            //Appointment appointment = appointmentService.cancelAppointment()
+        }
+        catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }*/
+        return null;
+    }
+}
