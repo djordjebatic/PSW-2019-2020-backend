78936b7b01fbb277b7f04ee23943aac8a0c6a868
Jelena Dostić <49482144+DosticJelena@users.noreply.github.com>
2019-12-21 23:29:15 +0100

    chaged page layout

--- src/App/Content/Doctor/Doctor.js
+++ src/App/Content/Doctor/Doctor.js
@@ -10,62 +10,65 @@ import logo from '../../../images/med128.png'
 
 class Doctor extends React.Component {
 
-    constructor(props){
-        super(props); 
-    
+    constructor(props) {
+        super(props);
+
         this.state = {
             firstName: '',
             lastName: ''
         }
     }
 
-    componentDidMount(){
-    var token = localStorage.getItem('token');
-    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-    axios.get("http://localhost:8080/auth/getMyUser")  
-        .then(response => {
-            console.log(response.data);
-            this.setState({
-                firstName: response.data.firstName,
-                lastName: response.data.lastName
+    componentDidMount() {
+        var token = localStorage.getItem('token');
+        axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+        axios.get("http://localhost:8080/auth/getMyUser")
+            .then(response => {
+                console.log(response.data);
+                this.setState({
+                    firstName: response.data.firstName,
+                    lastName: response.data.lastName
+                })
             })
-        })
-    .catch((error) => console.log(error))
+            .catch((error) => console.log(error))
     }
 
-  render() {
-      return (
-        <div className="Doctor">
-        <Header/>
-        <div className="">
-            <div className="row welcome-doctor">
-                <div className="col-12">
-                        <div className="logo-doctor">
-                            <img src={logo} alt="logo" />
+    render() {
+        return (
+            <div className="Doctor">
+                <Header />
+                <div className="">
+                    <div className="row welcome-doctor">
+                        <div className="col-12">
+                            <div className="logo-doctor">
+                                <img src={logo} alt="logo" />
+                            </div>
+                            <h3 className="welcome-and-logo">Welcome, {this.state.firstName} {this.state.lastName}</h3>
                         </div>
-                        <h3 className="welcome-and-logo">Welcome, {this.state.firstName} {this.state.lastName}</h3>
+                    </div>
+                    <div className="row links">
+                        <div className="col link">
+                            <h4>All Patients</h4>
+                            <p>Look at the list of all patients in your clinic.</p>
+                            <Link to="/patients" className="btn link-btn-doctor">View List</Link>
+                        </div>
+                        <div className="col link">
+                            <h4>Create New Appointment Request</h4>
+                            <p>Choose date, ordination and type of the appointment.</p>
+                            <Link to="/new-appointment-doctor" className="btn link-btn-doctor">Create</Link>
+                            <br/>
+                            <br/>
+                            <Link to="/reservation-requests" className="btn link-btn-doctor">List of created requests</Link>
+                        </div>
+                        <div className="col link">
+                            <h4>Create Absence/Vacation Request</h4>
+                            <p>Choose start and end date.</p>
+                            <Link to="/absence-request" className="btn link-btn-doctor">Create</Link>
+                        </div>
+                    </div>
                 </div>
+                <Footer />
             </div>
-            <div className="row links">
-                <div className="col link">
-                    <h4>All Patients</h4>
-                    <p>Look at the list of all patients in your clinic.</p>
-                    <Link to="/patients" className="btn link-btn-doctor">View List</Link>
-                </div>
-                <div className="col link">
-                    <h4>Create New Appointment</h4>
-                    <p>Choose date, ordination and type of the appointment.</p>
-                    <Link to="/new-appointment-doctor" className="btn link-btn-doctor">Create</Link>
-                </div>
-                <div className="col link">
-                    <h4>Create Absence/Vacation Request</h4>
-                    <p>Choose start and end date.</p>
-                    <Link to="/absence-request" className="btn link-btn-doctor">Create</Link>
-                </div>
-            </div>
-        </div>
-        <Footer/>
-        </div>
         );
     }
 }
