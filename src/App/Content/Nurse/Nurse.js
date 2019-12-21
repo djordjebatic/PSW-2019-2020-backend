4ffb2862c19afe22ebe530679e8a18e1001a1d30
Jelena Dostić <49482144+DosticJelena@users.noreply.github.com>
2019-12-21 23:30:24 +0100

    changed page layout

--- src/App/Content/Nurse/Nurse.js
+++ src/App/Content/Nurse/Nurse.js
@@ -1,46 +1,71 @@
 import React from 'react';
 import './Nurse.css'
 import "react-table/react-table.css";
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
+import axios from 'axios';
 
 
 import logo from '../../../images/med128.png'
 
 class Nurse extends React.Component {
-  render() {
-    
-  return (
-    <div className="Nurse">
-      <Header/>
-        <div className="">
-            <div className="row welcome-nurse">
-                <div className="col-12">
-                        <div className="logo-nurse">
-                            <img src={logo} alt="logo" />
-                        </div>
-                        <h3 className="welcome-and-logo">Welcome, Nurse</h3>
-                </div>
-            </div>
-            <div className="row links">
-                <div className="col link">
-                    <h4>All Patients</h4>
-                    <p>Look at the list of all patients in your clinic.</p>
-                    <Link to="/patients" class="btn link-btn-doctor">View List</Link>
-                </div>
-                <div className="col link">
-                    <h4>Prescriptions to Authenticate</h4>
-                    <p>Look at the list of all prescriptions.</p>
-                    <a href="#" className="btn link-btn-doctor">View List</a>
-                </div>
-            </div>
-        </div>
-        <Footer/>
 
-    </div>
-  );
-  }
+    constructor(props){
+        super(props); 
+    
+        this.state = {
+            firstName: '',
+            lastName: ''
+        }
+    }
+
+    componentDidMount() {
+        var token = localStorage.getItem('token');
+        axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+        axios.get("http://localhost:8080/auth/getMyUser")
+            .then(response => {
+                console.log(response.data);
+                this.setState({
+                    firstName: response.data.firstName,
+                    lastName: response.data.lastName
+                })
+            })
+            .catch((error) => console.log(error))
+    }
+
+    render() {
+
+        return (
+            <div className="Nurse">
+                <Header />
+                <div className="">
+                    <div className="row welcome-nurse">
+                        <div className="col-12">
+                            <div className="logo-nurse">
+                                <img src={logo} alt="logo" />
+                            </div>
+                            <h3 className="welcome-and-logo">Welcome, {this.state.firstName} {this.state.lastName}</h3>
+                        </div>
+                    </div>
+                    <div className="row links">
+                        <div className="col link">
+                            <h4>All Patients</h4>
+                            <p>Look at the list of all patients in your clinic.</p>
+                            <Link to="/patients" class="btn link-btn-doctor">View List</Link>
+                        </div>
+                        <div className="col link">
+                            <h4>Prescriptions to Authenticate</h4>
+                            <p>Look at the list of all prescriptions.</p>
+                            <a href="#" className="btn link-btn-doctor">View List</a>
+                        </div>
+                    </div>
+                </div>
+                <Footer />
+
+            </div>
+        );
+    }
 }
 
 export default Nurse;
