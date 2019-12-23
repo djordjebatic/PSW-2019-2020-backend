1a8cc178ab4ce9d005ada91ecbbc7bb285d93f28
Djordje Batic <djordjebatic@gmail.com>
2019-12-23 22:47:12 +0100

    Create ClinicAdminServiceImpl.java

---
+++ psw-backend/src/main/java/com/example/pswbackend/serviceImpl/ClinicAdminServiceImpl.java
@@ -0,0 +1,124 @@
+package com.example.pswbackend.serviceImpl;
+
+import com.example.pswbackend.domain.*;
+import com.example.pswbackend.dto.AppointmentDoctorDTO;
+import com.example.pswbackend.dto.ClinicAdminDTO;
+import com.example.pswbackend.dto.QuickReservationDTO;
+import com.example.pswbackend.enums.AppointmentEnum;
+import com.example.pswbackend.enums.UserStatus;
+import com.example.pswbackend.repositories.ClinicAdminRepository;
+import com.example.pswbackend.repositories.ClinicRepository;
+import com.example.pswbackend.repositories.DoctorRepository;
+import com.example.pswbackend.repositories.OrdinationRepository;
+import com.example.pswbackend.services.AppointmentRequestService;
+import com.example.pswbackend.services.ClinicAdminService;
+import com.example.pswbackend.services.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ClinicAdminServiceImpl implements ClinicAdminService {
+
+    @Autowired
+    ClinicAdminRepository clinicAdminRepository;
+
+    @Autowired
+    ClinicRepository clinicRepository;
+
+    @Autowired
+    DoctorRepository doctorRepository;
+
+    @Autowired
+    OrdinationRepository ordinationRepository;
+
+    @Autowired
+    AppointmentRequestService appointmentRequestService;
+
+    @Autowired
+    EmailService emailService;
+
+    @Override
+    public ClinicAdminDTO findByName(String name) {
+        ClinicAdminDTO clinicAdminDTO = clinicAdminRepository.findByEmail(name);
+        if (clinicAdminDTO == null) {
+            return null;
+        }
+
+        return new ClinicAdminDTO(clinicAdminDTO.getEmail(), clinicAdminDTO.getPassword(), clinicAdminDTO.getFirstName(), clinicAdminDTO.getLastName(), clinicAdminDTO.getPhoneNumber(), clinicAdminDTO.getAddress(), clinicAdminDTO.getCity(), clinicAdminDTO.getCountry(), clinicAdminDTO.getClinicId());
+    }
+
+    @Override
+    public ClinicAdmin register(ClinicAdminDTO clinicAdminDTO) {
+
+        ClinicAdmin clinicAdmin = new ClinicAdmin();
+        clinicAdmin.setFirstName(clinicAdminDTO.getFirstName());
+        clinicAdmin.setLastName(clinicAdminDTO.getLastName());
+        clinicAdmin.setPhoneNumber(clinicAdminDTO.getPhoneNumber());
+        clinicAdmin.setEmail(clinicAdminDTO.getEmail());
+        clinicAdmin.setAddress(clinicAdminDTO.getAddress());
+        clinicAdmin.setCity(clinicAdminDTO.getCity());
+        clinicAdmin.setCountry(clinicAdminDTO.getCountry());
+        clinicAdmin.setPassword(clinicAdminDTO.getPassword());
+        clinicAdmin.setClinic(clinicRepository.findOneById(clinicAdminDTO.getClinicId()));
+        clinicAdmin.setUserStatus(UserStatus.NEVER_LOGGED_IN);
+        if (clinicAdminRepository.findByEmail(clinicAdmin.getUsername()) != null){
+            System.out.println("Vec postoji");
+            return null;
+        }
+
+        String s = "You have been registered as an Admin of %s" + clinicAdmin.getClinic().getName() + " clinic! You can now log in to the Clinical Centre System";
+        //emailService.sendEmail(clinicAdmin.getEmail(), "Registration Request Response", s);
+
+        return clinicAdminRepository.save(clinicAdmin);
+
+    }
+
+    //TODO provera mogucnosti zakazivanja
+    @Override
+    public boolean receiveAppointmentRequest(AppointmentDoctorDTO dto) {
+
+        ClinicAdmin ca = clinicAdminRepository.findAll().get(0);
+        appointmentRequestService.saveRequest(dto, ca);
+
+        return true;
+    }
+
+    //TODO
+    @Override
+    public List<ClinicAdmin> findAll() {
+        return null;
+
+    }
+
+    @Override
+    public Appointment createQuickReservation(QuickReservationDTO dto){
+
+        // provere..............
+
+        Appointment predefinedAppointment = new Appointment();
+
+        AppointmentEnum appType;
+        if (dto.getType() == "0"){
+            appType = AppointmentEnum.EXAMINATION;
+        } else {
+            appType = AppointmentEnum.OPERATION;
+        }
+
+        System.out.println("-------------------- ORDINATION: " + dto.getDoctor());
+
+        //predefinedAppointment.setDate(dto.getDate());
+        //predefinedAppointment.setTime(dto.getTime());
+        predefinedAppointment.setOrdination(ordinationRepository.findById(Long.parseLong(dto.getOrdination())).get());
+        //predefinedAppointment.setDoctor(doctorRepository.findById(Long.parseLong(dto.getDoctor())).get());
+        //predefinedAppointment.setDuration(dto.getDuration());
+        predefinedAppointment.getPrice().setPrice(dto.getPrice());
+        predefinedAppointment.getPrice().setAppointmentEnum(appType);
+
+        //TODO dodati u listu predefinisanih appointmenta
+        /// dto.getClinicAdmin().getPredefinedAppointmetns().push/add/saveNew...
+
+        return predefinedAppointment;
+    }
+}
