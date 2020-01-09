fc200a27c34603dc560ebeea32dfa5c86012e182
SaraFojkar <56953463+SaraFojkar@users.noreply.github.com>
2020-01-09 15:46:03 +0100

    method for loading certain clinic page

--- src/App/Content/Clinic/ClinicPage.js
+++ src/App/Content/Clinic/ClinicPage.js
@@ -4,8 +4,33 @@ import "react-table/react-table.css";
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import './ClinicPage.css'
+import axios from 'axios'
 
 class ClinicPage extends React.Component {
+
+    constructor(props){
+        super(props); 
+    
+        this.state = {
+            id: '',
+            name: ''
+        }
+    }
+
+    componentDidMount(){
+        var token = localStorage.getItem('token');
+        axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;  
+         axios.get("http://localhost:8080/api/clinic/"+this.state.id)  
+            .then(response => {
+                console.log(response.data);
+                this.setState({
+                    id: response.data.id,
+                    name: response.data.name
+                })
+            })
+        .catch((error) => console.log(error))
+        }
+
     render() {
         return(
             
@@ -13,9 +38,14 @@ class ClinicPage extends React.Component {
                 <Header/>
 
                 <div className="clinic-page-title">Clinic page</div>
+                <h3>{this.state.name}</h3>
 
                 <div className="btn-predefined-exam">
                     <Link to="/predefined-examinations" className="btn link-btn-patient predefined-btn">Predefined examinations</Link>
+                </div>
+
+                <div className="btn-predefined-exam">
+                    <Link to="/doctors-list" className="btn link-btn-patient predefined-btn">Doctors of the clinic</Link>
                 </div>
 
                 <Footer/>
