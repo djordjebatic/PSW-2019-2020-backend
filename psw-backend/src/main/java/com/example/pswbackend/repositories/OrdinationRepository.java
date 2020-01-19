b17c4de2fe8ea4dfada89930007805dd681782ad
Djordje Batic <djordjebatic@gmail.com>
2020-01-19 15:32:30 +0100

    Implemented regular, next day and automatic ordination assignment

--- psw-backend/src/main/java/com/example/pswbackend/repositories/OrdinationRepository.java
+++ psw-backend/src/main/java/com/example/pswbackend/repositories/OrdinationRepository.java
@@ -3,7 +3,11 @@ package com.example.pswbackend.repositories;
 import com.example.pswbackend.domain.Ordination;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrdinationRepository extends JpaRepository<Ordination, Long> {
 
+    Ordination findOneById(Long id);
+    List<Ordination> findByClinicId(Long id);
 
 }
