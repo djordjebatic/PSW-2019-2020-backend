2d18ad60ff4a6d48a7ca86c53234a41f157cfbc8
Djordje Batic <djordjebatic@gmail.com>
2020-01-19 15:37:37 +0100

    enable async email sending

--- psw-backend/src/main/java/com/example/pswbackend/serviceImpl/EmailServiceImpl.java
+++ psw-backend/src/main/java/com/example/pswbackend/serviceImpl/EmailServiceImpl.java
@@ -4,6 +4,7 @@ import com.example.pswbackend.services.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,6 +14,7 @@ public class EmailServiceImpl implements EmailService {
     private JavaMailSender javaMailSender;
 
     @Override
+    @Async
     public void sendEmail(String recipient, String subject, String message) {
         SimpleMailMessage mail = new SimpleMailMessage();
         mail.setTo(recipient);
