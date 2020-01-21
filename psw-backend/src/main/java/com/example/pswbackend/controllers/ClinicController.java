ad0b19d9db1703713716858dcace7627826a9c9c
SaraFojkar <56953463+SaraFojkar@users.noreply.github.com>
2020-01-21 12:49:55 +0100

    added get clinic method and filter clincs post method

--- psw-backend/src/main/java/com/example/pswbackend/controllers/ClinicController.java
+++ psw-backend/src/main/java/com/example/pswbackend/controllers/ClinicController.java
@@ -2,6 +2,7 @@ package com.example.pswbackend.controllers;
 
 import com.example.pswbackend.domain.Clinic;
 import com.example.pswbackend.dto.ClinicDTO;
+import com.example.pswbackend.dto.FilterClinicsDTO;
 import com.example.pswbackend.services.ClinicService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -36,10 +37,22 @@ public class ClinicController {
         return new ResponseEntity<>(lc, HttpStatus.OK);
     }
 
-    @GetMapping(value = "/clinic/{clinicId}",produces = MediaType.APPLICATION_JSON_VALUE)
-    @PreAuthorize("hasAuthority('PATIENT')")
-    public ClinicDTO getClinic(@PathVariable long clinicId) {
-        return this.clinicService.findById(clinicId);
+    @GetMapping(value = "/clinic/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
+    //@PreAuthorize("hasAuthority('PATIENT')")
+    public ResponseEntity<ClinicDTO> getClinic(@PathVariable long id) {
+        return new ResponseEntity<>(clinicService.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(value="/filter-clinics", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PreAuthorize("hasRole('PATIENT')")
+    public ResponseEntity<List<Clinic>> filterClinics(@RequestBody FilterClinicsDTO dto){
+
+        if (clinicService.filterClinics(dto)==null){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            List<Clinic> lc= clinicService.filterClinics(dto);
+            return new ResponseEntity<>(lc, HttpStatus.OK);
+        }
     }
     
 
