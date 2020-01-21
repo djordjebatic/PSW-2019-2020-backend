edddb94723c8770e8edc73b5f5d74cd6131120db
SaraFojkar <56953463+SaraFojkar@users.noreply.github.com>
2020-01-21 12:21:45 +0100

    Added predefined appointment DTO

---
+++ psw-backend/src/main/java/com/example/pswbackend/dto/PredefinedAppointmentDTO.java
@@ -0,0 +1,122 @@
+package com.example.pswbackend.dto;
+
+import com.example.pswbackend.domain.Appointment;
+import com.example.pswbackend.domain.Doctor;
+
+import java.time.ZoneId;
+import java.util.Date;
+
+public class PredefinedAppointmentDTO {
+
+    private Long id;
+    private String type;
+    //private Date date;
+    private Date startTime;
+    private Date endTime;
+    private String price;
+    private String duration;
+    private String doctors;
+    private String ordination;
+    private String discount;
+
+    public PredefinedAppointmentDTO(Appointment appointment){
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Doctor d : appointment.getDoctors()){
+            stringBuilder.append(d.getFirstName() + " " + d.getLastName() + " ");
+        }
+        String doctors = stringBuilder.toString();
+
+        this.id = appointment.getId(); //Date.from( localDateTime.atZone( ZoneId.systemDefault()).toInstant());
+        this.type = appointment.getPrice().getAppointmentEnum().toString();
+        //this.date=Date.from(appointment.getStartDateTime().atZone(ZoneId.systemDefault()).toInstant());
+        this.startTime = Date.from(appointment.getStartDateTime().atZone(ZoneId.systemDefault()).toInstant());
+        this.endTime = Date.from(appointment.getEndDateTime().atZone(ZoneId.systemDefault()).toInstant());
+        this.price=appointment.getPrice().getPrice().toString();
+        this.duration="0";
+        this.doctors = doctors;
+        this.ordination = appointment.getOrdination().getNumber();
+        this.discount=appointment.getDiscount().toString();
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    /*public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }*/
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public String getDoctors() {
+        return doctors;
+    }
+
+    public void setDoctors(String doctors) {
+        this.doctors = doctors;
+    }
+
+    public String getOrdination() {
+        return ordination;
+    }
+
+    public void setOrdination(String ordination) {
+        this.ordination = ordination;
+    }
+
+    public String getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(String discount) {
+        this.discount = discount;
+    }
+}
