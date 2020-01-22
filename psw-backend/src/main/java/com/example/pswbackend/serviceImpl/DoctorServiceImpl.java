383e8f237e2aeea5a00950da7624f2a747652500
Jelena Dostić <49482144+DosticJelena@users.noreply.github.com>
2020-01-22 18:26:59 +0100

    fixed package

--- psw-backend/src/main/java/com/example/pswbackend/serviceImpl/DoctorServiceImpl.java
+++ psw-backend/src/main/java/com/example/pswbackend/serviceImpl/DoctorServiceImpl.java
@@ -1,4 +1,4 @@
-package com.example.pswbackend.serviceImpl;
+package com.example.pswbackend.ServiceImpl;
 
 import com.example.pswbackend.domain.Account;
 import com.example.pswbackend.domain.Doctor;
