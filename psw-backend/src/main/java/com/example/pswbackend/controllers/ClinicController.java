469929e93765db4dd9517e1f2b641f8aaf49ad6b
SaraFojkar <56953463+SaraFojkar@users.noreply.github.com>
2020-01-23 15:39:44 +0100

    fixing methods for getting and filtering clinics

--- psw-backend/src/main/java/com/example/pswbackend/controllers/ClinicController.java
+++ psw-backend/src/main/java/com/example/pswbackend/controllers/ClinicController.java
@@ -3,6 +3,8 @@ package com.example.pswbackend.controllers;
 import com.example.pswbackend.domain.Clinic;
 import com.example.pswbackend.dto.ClinicDTO;
 import com.example.pswbackend.dto.FilterClinicsDTO;
+import com.example.pswbackend.dto.ResultClinicDTO;
+import com.example.pswbackend.repositories.ClinicRepository;
 import com.example.pswbackend.services.ClinicService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,6 +16,10 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -23,10 +29,12 @@ public class ClinicController {
     @Autowired
     ClinicService clinicService;
 
+    @Autowired
+    ClinicRepository clinicRepository;
+
     @GetMapping(value="/clinics", produces = MediaType.APPLICATION_JSON_VALUE)
     @PreAuthorize("hasRole('PATIENT')")
-    public ResponseEntity<List<Clinic>> getAllClinics(){
-
+    public ResponseEntity<List<ResultClinicDTO>> getAllClinics(){
 
         List<Clinic> lc= clinicService.findAll();
 
@@ -34,7 +42,13 @@ public class ClinicController {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
 
-        return new ResponseEntity<>(lc, HttpStatus.OK);
+        List<ResultClinicDTO> resultList= new ArrayList<>();
+        for(Clinic c : lc) {
+            ResultClinicDTO resultDTO = new ResultClinicDTO(c.getId().toString(), c.getName(), c.getDescription(), c.getAddress(), c.getCity(), Integer.toString(c.getStars()), Integer.toString(c.getNum_votes()), "");
+            resultList.add(resultDTO);
+        }
+
+        return new ResponseEntity<>(resultList, HttpStatus.OK);
     }
 
     @GetMapping(value = "/clinic/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
@@ -43,14 +57,18 @@ public class ClinicController {
         return new ResponseEntity<>(clinicService.findById(id), HttpStatus.OK);
     }
 
-    @PostMapping(value="/filter-clinics", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value="/filter-clinics/{date}/{type}", produces = MediaType.APPLICATION_JSON_VALUE)
     @PreAuthorize("hasRole('PATIENT')")
-    public ResponseEntity<List<Clinic>> filterClinics(@RequestBody FilterClinicsDTO dto){
+    public ResponseEntity<List<ResultClinicDTO>> filterClinics(@PathVariable String date, @PathVariable String type){
 
-        if (clinicService.filterClinics(dto)==null){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-d");
+        FilterClinicsDTO dto = new FilterClinicsDTO(type, LocalDate.parse(date, formatter));
+
+        if (clinicService.filterClinics(dto).isEmpty()){
+
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         } else {
-            List<Clinic> lc= clinicService.filterClinics(dto);
+            List<ResultClinicDTO> lc= clinicService.filterClinics(dto);
             return new ResponseEntity<>(lc, HttpStatus.OK);
         }
     }
