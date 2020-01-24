f5e4b19fdaa3bd4de5341ecf5fbe5afb3c411d2a
SaraFojkar <56953463+SaraFojkar@users.noreply.github.com>
2020-01-24 19:09:19 +0100

    added DTOS for filtering doctors from clinic

---
+++ psw-backend/src/main/java/com/example/pswbackend/dto/ResultDoctorDTO.java
@@ -0,0 +1,76 @@
+package com.example.pswbackend.dto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class ResultDoctorDTO {
+
+    String id;
+    String firstName;
+    String lastName;
+    String rating;
+    List<LocalDateTime> freeTerms;
+    List<String> free;
+
+    public ResultDoctorDTO() {
+    }
+
+    public ResultDoctorDTO(String id,String firstName, String lastName, String rating, List<LocalDateTime> freeTerms, List<String> free) {
+        this.id=id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.rating = rating;
+        this.freeTerms = freeTerms;
+        this.free= free;
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String stars) {
+        this.rating = rating;
+    }
+
+
+    public List<LocalDateTime> getFreeTerms() {
+        return freeTerms;
+    }
+
+    public void setFreeTerms(List<LocalDateTime> freeTerms) {
+        this.freeTerms = freeTerms;
+    }
+
+    public List<String> getFree() {
+        return free;
+    }
+
+    public void setFree(List<String> free) {
+        this.free = free;
+    }
+}
