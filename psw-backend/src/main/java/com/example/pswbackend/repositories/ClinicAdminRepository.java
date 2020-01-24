979f3ddc8341f11c928cf38ea1268f65fe0ffeeb
Jelena Dostić <49482144+DosticJelena@users.noreply.github.com>
2020-01-24 22:15:51 +0100

    additional method
    - long insted of Long (some error)

--- psw-backend/src/main/java/com/example/pswbackend/repositories/ClinicAdminRepository.java
+++ psw-backend/src/main/java/com/example/pswbackend/repositories/ClinicAdminRepository.java
@@ -9,6 +9,7 @@ import java.util.List;
 public interface ClinicAdminRepository extends JpaRepository <ClinicAdmin, Long> {
 
     ClinicAdmin findOneById(Long id);
+    ClinicAdmin findOneById(long id);
     ClinicAdminDTO findByEmail(String email);
 
     List<ClinicAdmin> findAll();
