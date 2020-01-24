43cd030a3a0fcb07a3f5cbc8a4a52e8804846c12
SaraFojkar <56953463+SaraFojkar@users.noreply.github.com>
2020-01-24 19:57:59 +0100

    Update PredefinedAppointmentDTO.java

--- psw-backend/src/main/java/com/example/pswbackend/dto/PredefinedAppointmentDTO.java
+++ psw-backend/src/main/java/com/example/pswbackend/dto/PredefinedAppointmentDTO.java
@@ -10,7 +10,6 @@ public class PredefinedAppointmentDTO {
 
     private Long id;
     private String type;
-    //private Date date;
     private Date startTime;
     private Date endTime;
     private String price;
@@ -29,7 +28,6 @@ public class PredefinedAppointmentDTO {
 
         this.id = appointment.getId(); //Date.from( localDateTime.atZone( ZoneId.systemDefault()).toInstant());
         this.type = appointment.getPrice().getAppointmentEnum().toString();
-        //this.date=Date.from(appointment.getStartDateTime().atZone(ZoneId.systemDefault()).toInstant());
         this.startTime = Date.from(appointment.getStartDateTime().atZone(ZoneId.systemDefault()).toInstant());
         this.endTime = Date.from(appointment.getEndDateTime().atZone(ZoneId.systemDefault()).toInstant());
         this.price=appointment.getPrice().getPrice().toString();
@@ -55,14 +53,6 @@ public class PredefinedAppointmentDTO {
     public void setType(String type) {
         this.type = type;
     }
-
-    /*public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }*/
 
     public Date getEndTime() {
         return endTime;
