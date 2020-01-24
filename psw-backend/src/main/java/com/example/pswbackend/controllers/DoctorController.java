970433f5cdcf92a462fade1ae28ee64d114d51cd
SaraFojkar <56953463+SaraFojkar@users.noreply.github.com>
2020-01-24 19:08:23 +0100

    updated method for getting doctors from clinic and added method for filtering

--- psw-backend/src/main/java/com/example/pswbackend/controllers/DoctorController.java
+++ psw-backend/src/main/java/com/example/pswbackend/controllers/DoctorController.java
@@ -4,8 +4,7 @@ import com.example.pswbackend.domain.Account;
 import com.example.pswbackend.domain.Appointment;
 import com.example.pswbackend.domain.Doctor;
 import com.example.pswbackend.domain.Patient;
-import com.example.pswbackend.dto.AppointmentDoctorDTO;
-import com.example.pswbackend.dto.ChangePasswordDTO;
+import com.example.pswbackend.dto.*;
 import com.example.pswbackend.services.CustomAccountDetailsService;
 import com.example.pswbackend.services.DoctorService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +14,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -53,9 +56,25 @@ public class DoctorController {
     }
 
     @GetMapping(value="/doctors-list/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Doctor> getClinicDoctors(@PathVariable long id) {
+    @PreAuthorize("hasRole('PATIENT')")
+    public ResponseEntity<List<ResultDoctorDTO>> getClinicDoctors(@PathVariable long id) {
 
-        return doctorService.findByClinicId(id);
+            List<Doctor> doctorList =doctorService.findByClinicId(id);
+
+            if(doctorList==null){
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+
+            List<ResultDoctorDTO> resultList= new ArrayList<>();
+            List<LocalDateTime> ldt = new ArrayList<>();
+            List<String> free= new ArrayList<>();
+            for(Doctor d : doctorList) {
+                int r= d.getStars()/d.getNum_votes();
+                ResultDoctorDTO resultDTO = new ResultDoctorDTO(d.getId().toString(), d.getFirstName(), d.getLastName(), Integer.toString(r), ldt, free);
+                resultList.add(resultDTO);
+            }
+
+            return new ResponseEntity<>(resultList, HttpStatus.OK);
     }
 
     @RequestMapping(value = "/doctor/change-password", method = RequestMethod.POST)
@@ -69,6 +88,22 @@ public class DoctorController {
     static class PasswordChanger {
         public String oldPassword;
         public String newPassword;
+    }
+
+    @GetMapping(value="/filter-doctors/{date}/{type}/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @PreAuthorize("hasRole('PATIENT')")
+    public ResponseEntity<List<ResultDoctorDTO>> filterDoctors(@PathVariable String date, @PathVariable String type, @PathVariable String id){
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-d");
+        FilterDoctorsDTO dto = new FilterDoctorsDTO(LocalDate.parse(date, formatter),type, id);
+
+
+        if (doctorService.filterDoctors(dto).isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            List<ResultDoctorDTO> lc= doctorService.filterDoctors(dto);
+            return new ResponseEntity<>(lc, HttpStatus.OK);
+        }
     }
 
 }
