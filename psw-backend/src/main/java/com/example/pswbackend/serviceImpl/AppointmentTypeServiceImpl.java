5efcc683429467026eafbb647a5211c770b24d54
Jelena Dostić <49482144+DosticJelena@users.noreply.github.com>
2020-02-01 23:53:26 +0100

    added methods for creating new app. type and updating app. type - controller

--- psw-backend/src/main/java/com/example/pswbackend/serviceImpl/AppointmentTypeServiceImpl.java
+++ psw-backend/src/main/java/com/example/pswbackend/serviceImpl/AppointmentTypeServiceImpl.java
@@ -1,12 +1,18 @@
 package com.example.pswbackend.ServiceImpl;
 
 import com.example.pswbackend.domain.AppointmentType;
+import com.example.pswbackend.domain.Clinic;
 import com.example.pswbackend.dto.AppointmentTypeDTO;
+import com.example.pswbackend.enums.AppointmentEnum;
 import com.example.pswbackend.repositories.AppointmentTypeRepository;
 import com.example.pswbackend.services.AppointmentTypeService;
+import com.example.pswbackend.services.ClinicService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,6 +22,9 @@ public class AppointmentTypeServiceImpl implements AppointmentTypeService {
     @Autowired
     private AppointmentTypeRepository appointmentTypeRepository;
 
+    @Autowired
+    private ClinicService clinicService;
+
     @Override
     public AppointmentTypeDTO findById(Long id) {
         AppointmentType appointmentType = appointmentTypeRepository.findOneById(id);
@@ -24,6 +33,37 @@ public class AppointmentTypeServiceImpl implements AppointmentTypeService {
         }
 
         return new AppointmentTypeDTO(appointmentType.getId(), appointmentType.getName());
+    }
+
+    @Override
+    @Transactional(readOnly = false, propagation = Propagation.REQUIRES_NEW)
+    public AppointmentType addNew(AppointmentTypeDTO dto){
+
+        try {
+            Long clinicId = dto.getClinicId();
+            Clinic c = clinicService.findClinicById(clinicId);
+            AppointmentType type = new AppointmentType(dto.getName(), c);
+            c.getAppointmentTypes().add(type);
+
+            return type;
+        } catch (Exception e){
+            return null;
+        }
+    }
+
+    @Override
+    @Transactional(readOnly = false, propagation = Propagation.REQUIRES_NEW)
+    public Boolean updateType(AppointmentType type, AppointmentTypeDTO dto){
+
+        try{
+            type.setName(dto.getName());
+            appointmentTypeRepository.save(type);
+        }
+        catch(Exception e){
+            return false;
+        }
+
+        return true;
     }
 
     @Override
