ece7de4c7a3a756a904ba96c35b2b82695505fa2
Jelena Dostić <49482144+DosticJelena@users.noreply.github.com>
2020-02-02 18:42:14 +0100

    added some fields

--- psw-backend/src/main/java/com/example/pswbackend/dto/PaidTimeOffNurseDTO.java
+++ psw-backend/src/main/java/com/example/pswbackend/dto/PaidTimeOffNurseDTO.java
@@ -3,14 +3,23 @@ package com.example.pswbackend.dto;
 import com.example.pswbackend.domain.Nurse;
 import com.example.pswbackend.enums.PaidTimeOffStatus;
 import com.example.pswbackend.enums.PaidTimeOffType;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.time.LocalDateTime;
 
 public class PaidTimeOffNurseDTO {
 
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    //@JsonFormat(pattern = "dd-MM-yyyy HH:mm")
     private LocalDateTime startDateTime;
+    //@JsonFormat(pattern = "dd-MM-yyyy HH:mm")
     private LocalDateTime endDateTime;
     private PaidTimeOffType paidTimeOffType;
     private String comment;
+    private String denialComment;
 
     public PaidTimeOffNurseDTO(){
 
@@ -30,5 +39,61 @@ public class PaidTimeOffNurseDTO {
 
     public String getComment() {
         return comment;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setStartDateTime(LocalDateTime startDateTime) {
+        this.startDateTime = startDateTime;
+    }
+
+    public void setEndDateTime(LocalDateTime endDateTime) {
+        this.endDateTime = endDateTime;
+    }
+
+    public void setPaidTimeOffType(PaidTimeOffType paidTimeOffType) {
+        this.paidTimeOffType = paidTimeOffType;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDenialComment() {
+        return denialComment;
+    }
+
+    public void setDenialComment(String denialComment) {
+        this.denialComment = denialComment;
     }
 }
