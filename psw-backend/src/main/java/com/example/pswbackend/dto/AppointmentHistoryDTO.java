593f5def9f888e7649b05ed32af7f8a4412e8056
SaraFojkar <56953463+SaraFojkar@users.noreply.github.com>
2020-02-04 15:39:51 +0100

    updated appointmentHistoryDTO

--- psw-backend/src/main/java/com/example/pswbackend/dto/AppointmentHistoryDTO.java
+++ psw-backend/src/main/java/com/example/pswbackend/dto/AppointmentHistoryDTO.java
@@ -8,14 +8,18 @@ public class AppointmentHistoryDTO {
     private String doctorLastName;
     private String type;
     private String specialization;
+    private String diagnosis;
+    private Long id;
 
-    public AppointmentHistoryDTO(String startTime, String endTime, String doctorFirstName, String doctorLastName, String type, String specialization) {
+    public AppointmentHistoryDTO(String startTime, String endTime, String doctorFirstName, String doctorLastName, String type, String specialization, String diagnosis, Long id) {
         this.startTime = startTime;
         this.endTime = endTime;
         this.doctorFirstName = doctorFirstName;
         this.doctorLastName = doctorLastName;
         this.type = type;
         this.specialization = specialization;
+        this.diagnosis= diagnosis;
+        this.id=id;
     }
 
     public String getStartTime() {
@@ -64,5 +68,21 @@ public class AppointmentHistoryDTO {
 
     public void setSpecialization(String specialization) {
         this.specialization = specialization;
+    }
+
+    public String getDiagnosis() {
+        return diagnosis;
+    }
+
+    public void setDiagnosis(String diagnosis) {
+        this.diagnosis = diagnosis;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }
