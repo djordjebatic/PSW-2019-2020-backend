4665c572a4a14ead4f2c4091df503a808f7d5e7a
Jelena Dostić <49482144+DosticJelena@users.noreply.github.com>
2020-02-05 00:51:29 +0100

    ordination

--- psw-backend/src/main/java/com/example/pswbackend/repositories/OrdinationRepository.java
+++ psw-backend/src/main/java/com/example/pswbackend/repositories/OrdinationRepository.java
@@ -1,7 +1,9 @@
 package com.example.pswbackend.repositories;
 
 import com.example.pswbackend.domain.Ordination;
+import com.example.pswbackend.enums.AppointmentEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -9,5 +11,11 @@ public interface OrdinationRepository extends JpaRepository<Ordination, Long> {
 
     Ordination findOneById(Long id);
     List<Ordination> findByClinicId(Long clinicId);
+
+    //@Query(value = "SELECT * from Ordination o WHERE o.type = ?1", nativeQuery = true)
+    List<Ordination> findByTypeAndClinicId(AppointmentEnum type, Long clinicId);
+
+    @Query(value="DELETE FROM ordination o WHERE o.id = :id", nativeQuery = true)
+    Boolean deleteOneById(Long id);
 
 }
