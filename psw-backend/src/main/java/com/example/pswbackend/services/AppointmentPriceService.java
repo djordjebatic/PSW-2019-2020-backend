748a71ae233e7fff3c17efdb558a20df9b1d9bba
Jelena Dostić <49482144+DosticJelena@users.noreply.github.com>
2020-02-05 00:50:17 +0100

    appointment price

--- psw-backend/src/main/java/com/example/pswbackend/services/AppointmentPriceService.java
+++ psw-backend/src/main/java/com/example/pswbackend/services/AppointmentPriceService.java
@@ -1,8 +1,13 @@
 package com.example.pswbackend.services;
 
+import com.example.pswbackend.domain.AppointmentPrice;
 import com.example.pswbackend.dto.AppointmentPriceDTO;
+import com.example.pswbackend.dto.AppointmentPriceFullDTO;
+
+import java.util.List;
 
 public interface AppointmentPriceService {
 
     AppointmentPriceDTO getAppointmentPrice(Long specializationId);
+    List<AppointmentPriceFullDTO> getAll();
 }
