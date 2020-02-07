066cc3c543f8afdb0310e1805cb6b30b4225f2e1
Djordje Batic <djordjebatic@gmail.com>
2020-02-07 13:19:50 +0100

    constants

---
+++ psw-backend/src/test/java/com/example/pswbackend/constants/AppointmentConstants.java
@@ -0,0 +1,23 @@
+package com.example.pswbackend.constants;
+import com.example.pswbackend.enums.AppointmentStatus;
+
+public class AppointmentConstants {
+
+    public static final String ADMIN_EMAIL = "cadmin1@gmail.com";
+
+    public static final String ADMIN_PASSWORD = "admin";
+
+    public static final int APPOINTMENT_NO = 3;
+
+    public static final Long APPOINTMENT_ID = 6L;
+
+    public static final AppointmentStatus APPOINTMENT_STATUS_APPROVED = AppointmentStatus.APPROVED;
+
+    public static final AppointmentStatus APPOINTMENT_STATUS_AWAITING_APPROVAL = AppointmentStatus.AWAITING_APPROVAL;
+
+    public static final AppointmentStatus APPOINTMENT_STATUS_PREDEF_BOOKED = AppointmentStatus.PREDEF_BOOKED;
+
+    public static final AppointmentStatus APPOINTMENT_STATUS_CANCELED = AppointmentStatus.CANCELED;
+
+}
+
