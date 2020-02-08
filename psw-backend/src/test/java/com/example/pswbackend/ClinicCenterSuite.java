1a1a34d81176c72c290d2a89dfd112cb3d399641
Djordje Batic <djordjebatic@gmail.com>
2020-02-08 22:36:29 +0100

    Update ClinicCenterSuite.java

--- psw-backend/src/test/java/com/example/pswbackend/ClinicCenterSuite.java
+++ psw-backend/src/test/java/com/example/pswbackend/ClinicCenterSuite.java
@@ -4,18 +4,21 @@ import com.example.pswbackend.controllers.AppointmentControllerUnitTest;
 import com.example.pswbackend.repositories.AppointmentRepositoryTest;
 import com.example.pswbackend.repositories.ClinicRepositoryTest;
 import com.example.pswbackend.repositories.OrdinationRepositoryTest;
+import com.example.pswbackend.services.AppointmentServiceTest;
+import com.example.pswbackend.services.DoctorServiceTest;
+import com.example.pswbackend.services.OrdinationServiceTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-/**
- * @author Djordje Batic
- */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
         AppointmentControllerUnitTest.class,
         AppointmentRepositoryTest.class,
         ClinicRepositoryTest.class,
         OrdinationRepositoryTest.class,
+        AppointmentServiceTest.class,
+        OrdinationServiceTest.class,
+        DoctorServiceTest.class
 })
 public class ClinicCenterSuite {
 }
